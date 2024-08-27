b7f44c0efce222cce1e58dfe90f2a8a8c0496685
huchenlei <chenlei.hu@mail.utoronto.ca>
2024-08-27 17:02:14 -0400

    Add fake_model.safetensors download

--- __init__.py
+++ __init__.py
@@ -1,3 +1,12 @@
 from .dev_nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
+import os
+import server
+from aiohttp import web
+
+@server.PromptServer.instance.routes.get("/devtools/fake_model.safetensors")
+async def fake_model(request):
+    file_path = os.path.join(os.path.dirname(__file__), "fake_model.safetensors")
+    return web.FileResponse(file_path)
+
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
