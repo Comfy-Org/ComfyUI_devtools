57b1e5ea5e5672e8a5b2e55f3245a72d099ea0bf
huchenlei <chenlei.hu@mail.utoronto.ca>
2024-09-13 08:58:59 +0900

    Add raise error with message node

--- dev_nodes.py
+++ dev_nodes.py
@@ -12,10 +12,28 @@ class ErrorRaiseNode:
         raise Exception("Error node was called!")
 
 
+class ErrorRaiseNodeWithMessage:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {"required": {"message": ("STRING", {"multiline": True})}}
+
+    RETURN_TYPES = ()
+    OUTPUT_NODE = True
+
+    FUNCTION = "raise_error"
+    CATEGORY = "DevTools"
+    DESCRIPTION = "Raise an error with message for development purposes"
+
+    def raise_error(self, message: str):
+        raise Exception(message)
+
+
 NODE_CLASS_MAPPINGS = {
     "DevToolsErrorRaiseNode": ErrorRaiseNode,
+    "DevToolsErrorRaiseNodeWithMessage": ErrorRaiseNodeWithMessage,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "DevToolsErrorRaiseNode": "Raise Error",
+    "DevToolsErrorRaiseNodeWithMessage": "Raise Error with Message",
 }
